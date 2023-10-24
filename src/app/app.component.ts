47c59d7fa00bf69b27e5ea3db6c32928a6335c90
shivrajpro <shivrajhulbatte@gmail.com>
2023-10-24 23:33:42 +0530

    using ViewChild to send data from child to parent cmp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { PostComponent } from './post/post.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'angular-refresher-2023';
+
+  @ViewChild(PostComponent) childComp = null;
+
+  constructor(){
+    console.log('>> constructor',this.childComp);
+  }
+  
+  ngOnInit(): void {
+    console.log('>> ngOnInit',this.childComp);
+  }
+
+  ngAfterViewInit(): void {
+    console.log('>> ngAfterViewInit',this.childComp);
+  }
 }
