56fcac15af5f13b12e82a4c65f4ef9a082bd2dbf
shivrajpro <shivrajhulbatte@gmail.com>
2023-10-24 23:30:25 +0530

    create navbar, post and post-list compnent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PostListComponent } from './post/post-list/post-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    NavbarComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/post/post-list/post-list.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.scss']
+})
+export class PostListComponent {
+  @Input() fromParent:string = '';
+}
