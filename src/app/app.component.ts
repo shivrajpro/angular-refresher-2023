8732601b63cdb90f1877129bb8e87822e946e7b2
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2023-11-25 12:19:59 +0530

    fix errors on cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,9 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { PostComponent } from './post/post.component';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit, AfterViewInit {
-  @ViewChild(PostComponent) childComp = null;
-
-  decVal = 3.85674;
-  constructor(private router: Router) {
-    // console.log('>> constructor',this.childComp);
-  }
-
-  ngOnInit(): void {
-    // console.log('>> ngOnInit',this.childComp);
-  }
-
-  ngAfterViewInit(): void {
-    // console.log('>> ngAfterViewInit',this.childComp);
-  }
-
-  onSubmit() {
-    // this.router.navigate(['posts/post', 1]);
-    this.router.navigate(['posts'], {
-      queryParams: { page: 1, orderBy: 'title' },
-    });
-  }
+export class AppComponent{
 }
