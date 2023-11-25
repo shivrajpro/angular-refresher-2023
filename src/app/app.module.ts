62f8f8e0cbe3f2abfe5d6ec5f15dadea5d2b5ba2
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2023-11-25 12:15:11 +0530

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostComponent } from './post/post.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { PostListComponent } from './post/post-list/post-list.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { PrefixPipe } from './pipes/prefix.pipe';
-import { SummaryPipe } from './pipes/summary.pipe';
-import { PostService } from './services/post.service';
-import { PostDetailComponent } from './post/post-detail/post-detail.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { SignalsComponent } from './signals/signals.component';
-import { CommonModule } from '@angular/common';
-import { LockInputComponent } from './lock-input/lock-input.component';
-import { ValueAccessorComponent } from './value-accessor-component/value-accessor-component.component';
-import { HighchartsChartModule } from 'highcharts-angular';
-import { HtmlToPdfComponent } from './html-to-pdf/html-to-pdf.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent,
-    NavbarComponent,
-    PostListComponent,
-    PrefixPipe,
-    SummaryPipe,
-    PostDetailComponent,
-    PageNotFoundComponent,
-    SignalsComponent,
-    LockInputComponent,
-    ValueAccessorComponent,
-    HtmlToPdfComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HighchartsChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
